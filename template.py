c5c379ea461cf450955f67eacffd58c445718347
GetDatCodekour <42021513+GetDatCodekour@users.noreply.github.com>
2018-12-06 09:26:21 +1100

    Update template.py

--- template.py
+++ template.py
@@ -5,6 +5,60 @@ import sqlalchemy
 from SQLiteConnection import engine, Session
 from ModelClasses import *
 
+from SQLite connection import engine,session
+
+
+
+
+
+
+
+
+
+session = session(autoflush=False)
+
+with open(args.input_file) as datafile:
+  
+  line=datafile.readlne()
+  while line.syarwith('#'):
+    line=datafile.readline()
+    
+   col_names=line.rstrip("\n").split('|')
+  
+  For line in datafile:
+    values=line.rstrip().split('|')
+    
+  data=dict(zip(col_names,values))
+  
+  student=Student()
+  student.first_name=data['first_name']
+  student.last_name=data['last_name']
+  session.add(student)
+  
+  try:
+    status = session.query(Stauus).filter(statis.label==daa['status']).one()
+  except sqlalchemy.orm.exc.NoResultFound:
+    #status found, create a new one
+    status=Status()
+    status.label=data['status']
+    session.add(status)
+  except sqlalchemy.orm.exc.MultipleResultsFound:
+    raise Exception("More than one result found in db - add a new status")
+    
+  student.status.append(status)
+  
+  #Same base code for city
+  
+  if len(data['club']) > 0:
+    for club_name in data['club'].split(", "):
+      try:
+        club=session.quey(Club).filter(Club.name==club_label)
+      except sqlalchemy.orm.exc.NoResultFound:
+        club=Club()
+        club.name=club_name
+        clud.id=club_id
+        session.add(club)
+        
 # filename = 'student_data.txt'
 # or use argparse
 
